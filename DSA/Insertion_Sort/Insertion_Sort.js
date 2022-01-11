58ed3a8fe3728835767f4139c5fdf4659a70dbfe
Udaya Kumar V <74949367+udaya38@users.noreply.github.com>
2022-01-11 19:25:57 +0530

    Insertion_sort uploaded

---
+++ DSA/Insertion_Sort/Insertion_Sort.js
@@ -0,0 +1,15 @@
+//Insertion Sort
+
+const insertionSort=(arr)=>{
+    for(let i=1;i<arr.length;i++){
+      let currVal=arr[i];
+      let j;
+      for(j=i-1;j>=0 && arr[j] > currVal;j--){
+        arr[j+1]=arr[j];
+      }
+      arr[j+1]=currVal
+    }
+    return arr;
+}
+
+console.log(insertionSort([4,5,3,8,6,7,1,0,-1,34,-34])) 
